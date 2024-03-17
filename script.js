83f2649b83bba6072e47fb07cba28c4b43f72ebb
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-03-17 15:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+const form = document.getElementById('form');
+const nameInput = document.getElementById('name');
+const yearInput = document.getElementById('year');
+const urlElement = document.getElementById('url');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = nameInput.value;
+    const year = yearInput.value;
+
+    let queryString = `?name=${name}&year=${year}`;
+    urlElement.textContent = `https://localhost:8080/${queryString}`;
+});
